f3ea2f19702be9ff8ac72de0c4440803729ff710
TryaNever <alexispernette@outlook.fr>
2025-04-14 15:03:42 +0200

    ADD new méthod all in CarController

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -37,4 +37,26 @@ class CarController extends Controller
             'typeGears' => $typeGear,
             'vehicules' => $vehicules,]);
     }
+
+    public function all() {
+        $vehicules = DB::table('vehicule')
+            ->join('vehicule_type', 'vehicule.vehicule_type_id', '=', 'vehicule_type.id')
+            ->join('vehicule_photo', 'vehicule.id', '=', 'vehicule_photo.vehicule_id')
+            ->select('vehicule.id',
+                'vehicule_photo.image_url as photo',
+                'vehicule.brand',
+                'vehicule.model',
+                'vehicule.transmission',
+                'vehicule.fuel_type',
+                'vehicule.price_per_day',
+                'vehicule_type.name as vehicule_type',
+                DB::raw("IF(vehicule.air_conditioning = 1, 'Air conditionné', 'Sans climatisation') as air_conditionne")
+            )
+            ->where('vehicule_photo.display_order', 0)
+            ->limit(6)
+            ->get();
+
+
+        return view('', ['vehicules' => $vehicules]);
+    }
 }
