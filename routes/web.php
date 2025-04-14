374612451e745f152187e0b48dffd3deaac20902
TryaNever <alexispernette@outlook.fr>
2025-04-14 22:23:59 +0200

    FINISH US-2-E TODO US-3-A

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [CarController::class, 'index']);
 Route::get('/vehicules', [CarController::class, 'all']);
 Route::post('/vehicules/filter', [CarController::class, 'filter']);
+Route::get('/vehicules/{vehicules_id}', [CarController::class, 'detail']);
 Route::get('/vehicules/{vehicules_id}/reservation', [CarController::class, 'reservation']);
