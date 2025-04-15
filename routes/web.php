d4b3c56fdcf755f6fd751cda46b3e4c6a734d16c
TryaNever <alexispernette@outlook.fr>
2025-04-15 15:29:44 +0200

    US-7 FINISH

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CarController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CarController::class, 'index']);
@@ -8,3 +9,4 @@ Route::get('/vehicules', [CarController::class, 'all']);
 Route::post('/vehicules/filter', [CarController::class, 'filter']);
 Route::get('/vehicules/{vehicules_id}', [CarController::class, 'detail']);
 Route::get('/vehicules/{vehicules_id}/reservation', [CarController::class, 'reservation']);
+Route::post('/reservation', [ReservationController::class, 'store']);
